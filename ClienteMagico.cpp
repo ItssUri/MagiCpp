9795cb886aa580551e42810f5be14c9d554e7dc3
uri <73711008+ItssUri@users.noreply.github.com>
2024-04-05 12:51:53 +0200

    Add files via upload

---
+++ ClienteMagico.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "MagiaEstatica.h"
+#include <windows.h> 
+#include <random>
+
+
+int main()
+{
+	srand(time(NULL));
+	MagiaEstatica::Telepatia::introduction(); 
+	MagiaEstatica::Telepatia::nameGathering();
+	MagiaEstatica::Telepatia::nameMagic();
+	MagiaEstatica::Telepatia::bdayGathering();
+	MagiaEstatica::Telepatia::zodiacMagic();
+	MagiaEstatica::Telepatia::ending();
+}
